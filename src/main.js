68089768d0a36fcf944f8dae9826c33c8df50f9a
Sujin <su_jp@naver.com>
2023-01-29 17:51:45 +0900

    axios 추가 설치

--- src/main.js
+++ src/main.js
@@ -2,11 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
+import axios from 'axios';
+
+Vue.prototype.$eventBus = new Vue();
+Vue.prototype.$axios = axios;
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   vuetify,
+  axios,
   render: h => h(App)
 }).$mount('#app')
